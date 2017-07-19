555d6d3cdb48500057b0f4f0f047dac943ecd6b9
cassese <cassese@users.noreply.github.com>
2017-07-18 22:32:36 -0500

    addbar function
    added the abillity to add bar accounts useing redbean!

--- addbar.php
+++ addbar.php
@@ -1,1 +1,31 @@
-<?php?>
+<?php
+
+include "rb.php";
+
+R::setup( 'mysql:host=localhost;dbname=barapp', 'barapp', 'password' );
+
+if(isset($_POST['submit'])){
+
+    $bar = R::find('bar', 'username="'.$_POST['username'].'"');
+
+    if(!isset($bar->barname)){
+
+        $bar = R::dispense('bar');
+
+        $bar->barname = $_POST['barname'];
+        $bar->username = $_POST['username'];
+        $bar->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $id = R::store( $bar );
+
+        echo "Account created!";
+
+    }else{
+
+        echo "Account not created for username is taken!";
+
+    }
+
+}
+
+?>
