ee0bd385124a5fb8b0ca04ce2b4a7a01d45ed08b
cassese <cassese@users.noreply.github.com>
2017-07-18 22:35:02 -0500

    adduser function
    added php script to add a normal user using redbean!

--- adduser.php
+++ adduser.php
@@ -1,1 +1,31 @@
-<?php?>
+<?php
+
+include "rb.php";
+
+R::setup( 'mysql:host=localhost;dbname=barapp', 'barapp', 'password' );
+
+if(isset($_POST['submit'])){
+
+    $user = R::find('user', 'username="'.$_POST['username'].'"');
+
+    if(!isset($user->username)){
+
+        $user = R::dispense('user');
+
+        $user->username = $_POST['username'];
+        $user->username = $_POST['username'];
+        $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $id = R::store( $user );
+
+        echo "Account created!";
+
+    }else{
+
+        echo "Account not created for username is taken!";
+
+    }
+
+}
+
+?>
